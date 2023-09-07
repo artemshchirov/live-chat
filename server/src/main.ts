3bafb2b942b7b51cf128980bb227217d7b2c98e4
Artem Shchirov <artemshchirov@gmail.com>
2023-09-07 18:05:34 +0300

    updates before deploy

--- server/src/main.ts
+++ server/src/main.ts
@@ -12,6 +12,6 @@ async function bootstrap() {
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
